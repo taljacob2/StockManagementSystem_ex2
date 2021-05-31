37afbae9c5459c4935dcc0d0fc3d08b5de78a345
TalJacob <taljacob@gmail.com>
2021-05-31 22:13:20 +0300

    after doc and organization of OrderExecution

--- javafx/src/application/pane/resources/orderexecution/OrderExecution.java
+++ javafx/src/application/pane/resources/orderexecution/OrderExecution.java
@@ -33,7 +33,36 @@ import java.util.stream.Collectors;
  */
 public class OrderExecution implements Initializable {
 
+    /**
+     * <p>A dynamical value.</p>
+     * <p>
+     * This value indicates the <i>minimum</i> <i>valid</i> {@code Quantity} in
+     * the executed {@code Order}.
+     * </p>
+     * <p>
+     * Mainly, this number is a {@code final} value of {@code 1}.
+     */
     private Long activeMinQuantity = 1L;
+
+    /**
+     * <p>A dynamical value.</p>
+     * <p>
+     * This value indicates the <i>maximum</i> <i>valid</i> {@code Quantity} in
+     * the executed {@code Order}.
+     * </p>
+     *
+     * <ul>
+     *     <li>In case the {@code Order} is a <i>Sell</i> {@code Order}, then
+     *     this number is set to be the <i>maximum</i> {@code Quantity} of
+     *     the current {@code Stock} being selected in the
+     *     {@link SelectedUser#getSelectedUser()} .
+     *     </li>
+     *     <li>In case the {@code Order} is a <i>Buy</i> {@code Order}, then
+     *     this number is set to be the {@code final}: {@link Long#MAX_VALUE}
+     *     number.
+     *     </li>
+     * </ul>
+     */
     private Long activeMaxQuantity = Long.MAX_VALUE;
 
     @FXML private ComboBox<String> buySellComboBox;
@@ -42,89 +71,57 @@ public class OrderExecution implements Initializable {
     @FXML private Button executeOrderButton;
     @FXML private TextField quantityTextField;
     @FXML private Label validityLabel;
-    /**
-     * Note: the when performing a "Buy" order, the 'max' limit of the {@code
-     * quantity} is hardcoded to be: '1000000'.
-     *
-     * @see #initQuantity
-     */
-    private SpinnerValueFactory<Integer> spinnerValueFactory;
     @FXML private Label userNameLabel;
 
 
     public OrderExecution() {}
 
     @Override public void initialize(URL location, ResourceBundle resources) {
+        initComboBoxes();
+        initUserNameLabel();
+        initTextQuantity();
+        initExecuteOrderButton();
+        initDisable();
+    }
+
+    private void initComboBoxes() {
         buySellComboBox.getItems().addAll("Buy", "Sell");
         orderTypeComboBox.getItems().addAll("LMT", "MKT");
-
 
         buySellComboBox.valueProperty().addListener(
                 (observable, oldValue, newValue) -> initStockComboBox());
 
         stockComboBox.valueProperty().addListener(
-                (observable, oldValue, newValue) -> initQuantity());
+                (observable, oldValue, newValue) -> initMinMaxQuantities());
+    }
 
+    private void initUserNameLabel() {
         userNameLabel.setText(
                 "Hello, " + SelectedUser.getSelectedUser().getName() + ". " +
                         "Please make an order.");
+    }
 
-        quantityTextField.setTextFormatter(new TextFormatter<>(change -> {
+    private void initDisable() {
 
-            // Allow only numbers:
-            if (change.getText().matches("[0-9]*")) {
-                return change;
-            }
-            return null;
-
-        }));
-
-        quantityTextField.textProperty()
-                .addListener((observable, oldValue, newValue) -> {
-                    try {
-                        if (Long.parseLong(newValue) > activeMaxQuantity) {
-                            quantityTextField.setText(oldValue);
-                        }
-
-                        if (Long.parseLong(newValue) < activeMinQuantity) {
-                            quantityTextField.setText(oldValue);
-                        }
-                    } catch (NumberFormatException e) {
-                        MessagePrint.println(MessagePrint.Stream.ERR,
-                                "Invalid [long] quantity.");
-                        return;
-                    }
-                    if (quantityTextField.textProperty().getValue()
-                            .matches("")) {
-                        MessagePrint.println(MessagePrint.Stream.ERR,
-                                "Invalid [long] quantity.");
-                        return;
-                    }
-                    MessagePrint.println(MessagePrint.Stream.OUT,
-                            "Valid [long] quantity.");
-                });
-
-        initExecuteOrderButton(JavaFXAppController.getStaticBorderPane(),
-                JavaFXAppController.getParentContainer(),
-                JavaFXAppController.getAnimationType());
-
-        /* -- Disable -- */
-
+        // 'stockComboBox' depends on 'buySellComboBox'.
         stockComboBox.disableProperty()
                 .bind(buySellComboBox.valueProperty().isNull()
                         .or(buySellComboBox.disableProperty()));
 
+
+        // 'orderTypeComboBox' depends on 'stockComboBox'.
         orderTypeComboBox.disableProperty()
                 .bind(stockComboBox.valueProperty().isNull()
                         .or(stockComboBox.disableProperty()));
 
+        // 'quantityTextField' depends on 'orderTypeComboBox'.
         quantityTextField.disableProperty()
                 .bind(orderTypeComboBox.valueProperty().isNull()
                         .or(orderTypeComboBox.disableProperty()));
 
         /*
-         * All combo-boxes must be selecting something,
-         * in order to press the button.
+         * All combo-boxes must be selecting something, in order to press the
+         * button. Thus, 'executeOrderButton' depends on all the combo-boxes.
          */
         executeOrderButton.disableProperty()
                 .bind(buySellComboBox.disableProperty()
@@ -134,7 +131,85 @@ public class OrderExecution implements Initializable {
                                                 .disableProperty()))));
     }
 
-    private void initQuantity() {
+    private void initTextQuantity() {
+        initTextQuantityFormatter();
+        initTextQuantityMinMaxValidation();
+    }
+
+    /**
+     * This method enforces that the given number in the {@link
+     * #quantityTextField} would be a <i>number</i>.
+     */
+    private void initTextQuantityFormatter() {
+        quantityTextField.setTextFormatter(new TextFormatter<>(change -> {
+
+            // Allow only numbers:
+            if (change.getText().matches("[0-9]*")) {
+                return change;
+            }
+            return null;
+
+        }));
+    }
+
+    /**
+     * This method checks whether the given number within {@link
+     * #quantityTextField} is a valid {@code long} number, that is in between
+     * the values of {@link #activeMinQuantity} and {@link #activeMaxQuantity}.
+     * <p>
+     * This method checks the given number in <i>real-time</i>.
+     * </p>
+     * <ul>
+     *      <li>In case the given number is <i>valid</i>, print an <i>output</i>
+     *      message to the {@link MessagePrint.Stream#OUT} {@code Stream}.
+     *      </li>
+     *      <li>In case the given number is <i>invalid</i>, print an <i>error</i>
+     *      message to the {@link MessagePrint.Stream#ERR} {@code Stream}.
+     *      </li>
+     * </ul>
+     */
+    private void initTextQuantityMinMaxValidation() {
+        quantityTextField.textProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    try {
+                        if (Long.parseLong(newValue) > activeMaxQuantity) {
+                            quantityTextField.setText(oldValue);
+                        }
+                        if (Long.parseLong(newValue) < activeMinQuantity) {
+                            quantityTextField.setText(oldValue);
+                        }
+                    } catch (NumberFormatException e) {
+
+                        // Means, the given number is invalid.
+                        printInvalidErrorMessage();
+                        return;
+                    }
+                    if (quantityTextField.textProperty().getValue()
+                            .matches("")) {
+
+                        // Means, there is no number given.
+                        printInvalidErrorMessage();
+                        return;
+                    }
+
+                    // If the given number is valid.
+                    printValidOutputMessage();
+                });
+    }
+
+    private void printInvalidErrorMessage() {
+        MessagePrint.println(MessagePrint.Stream.ERR,
+                "Invalid [long] 'Quantity'." + "\nNumber needs to be between:" +
+                        " '" + activeMinQuantity + "' and '" +
+                        activeMaxQuantity + "'");
+    }
+
+    private void printValidOutputMessage() {
+        MessagePrint
+                .println(MessagePrint.Stream.OUT, "Valid [long] 'Quantity'.");
+    }
+
+    private void initMinMaxQuantities() {
         if (buySellComboBox.valueProperty().getValue().toString()
                 .equals("Sell") &&
                 (stockComboBox.valueProperty().isNotNull().get())) {
@@ -160,9 +235,15 @@ public class OrderExecution implements Initializable {
         }
     }
 
-    private void initExecuteOrderButton(BorderPane borderPane,
-                                        Pane parentContainer,
-                                        PaneAnimator.AnimationType animationType) {
+    private void initExecuteOrderButton() {
+        setExecuteOrderButton(JavaFXAppController.getStaticBorderPane(),
+                JavaFXAppController.getParentContainer(),
+                JavaFXAppController.getAnimationType());
+    }
+
+    private void setExecuteOrderButton(BorderPane borderPane,
+                                       Pane parentContainer,
+                                       PaneAnimator.AnimationType animationType) {
 
         // define 'orderExecutionButton':
         executeOrderButton.setOnAction(
