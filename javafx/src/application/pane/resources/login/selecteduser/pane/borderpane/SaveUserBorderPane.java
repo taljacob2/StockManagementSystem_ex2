fbbf367bc58782506bb9b1e4de1026cfff722afa
TalJacob <taljacob@gmail.com>
2021-06-04 05:52:58 +0300

    bug fixed. need to organize fxml

--- javafx/src/application/pane/resources/login/selecteduser/pane/borderpane/SaveUserBorderPane.java
+++ javafx/src/application/pane/resources/login/selecteduser/pane/borderpane/SaveUserBorderPane.java
@@ -2,6 +2,12 @@ package application.pane.resources.login.selecteduser.pane.borderpane;
 
 import javafx.scene.layout.BorderPane;
 
+/**
+ * This class stores the {@link BorderPane} that is used as the {@code
+ * borderPaneToShowTheAnotherInnerPane} in the {@link application.pane.resources.login.selecteduser.pane.UserPane},
+ * for using it afterwards in {@link application.pane.resources.orderexecution.OrderExecution#setPane(BorderPane,
+ * String)}
+ */
 public class SaveUserBorderPane {
     private static BorderPane borderPaneToShowTheAnotherInnerPane;
 
@@ -9,8 +15,7 @@ public class SaveUserBorderPane {
         return borderPaneToShowTheAnotherInnerPane;
     }
 
-    public static void setBorderPaneToShowTheAnotherInnerPane(
-            BorderPane borderPaneToShowTheAnotherInnerPane) {
+    public static void setBorderPaneToShowTheAnotherInnerPane(BorderPane borderPaneToShowTheAnotherInnerPane) {
         SaveUserBorderPane.borderPaneToShowTheAnotherInnerPane =
                 borderPaneToShowTheAnotherInnerPane;
     }
